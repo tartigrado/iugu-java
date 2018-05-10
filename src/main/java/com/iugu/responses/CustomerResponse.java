eb3b28dfb752a129c0bf91c26f81e4752d4e8522
Daniel Pereira <danielpereiradonascimento@gmail.com>
2018-05-10 15:11:34 -0300

    Ajustes para adicinar campos na classe CustomerResponse.

--- src/main/java/com/iugu/responses/CustomerResponse.java
+++ src/main/java/com/iugu/responses/CustomerResponse.java
@@ -21,6 +21,17 @@ public class CustomerResponse {
     private List<CustomVariableResponse> customVariables;    
     @JsonProperty("cpf_cnpj")
     private String cpfCnpj;
+    @JsonProperty("zip_code")
+    private String zipCode;
+    private String number;
+    private String street;
+    private String district;
+    private String complement;
+    private String city;
+    private String state;
+    private String phone_prefix;
+    private String phone;
+    private String cc_emails;
     
     public String getId() {
         return id;
@@ -86,9 +97,89 @@ public class CustomerResponse {
         this.cpfCnpj = cpfCnpj;
     }
 
+    public String getZipCode() {
+        return zipCode;
+    }
+
+    public void setZipCode(String zipCode) {
+        this.zipCode = zipCode;
+    }
+
+    public String getNumber() {
+        return number;
+    }
+
+    public void setNumber(String number) {
+        this.number = number;
+    }
+
+    public String getStreet() {
+        return street;
+    }
+
+    public void setStreet(String street) {
+        this.street = street;
+    }
+
+    public String getDistrict() {
+        return district;
+    }
+
+    public void setDistrict(String district) {
+        this.district = district;
+    }
+
+    public String getComplement() {
+        return complement;
+    }
+
+    public void setComplement(String complement) {
+        this.complement = complement;
+    }
+
+    public String getCity() {
+        return city;
+    }
+
+    public void setCity(String city) {
+        this.city = city;
+    }
+
+    public String getState() {
+        return state;
+    }
+
+    public void setState(String state) {
+        this.state = state;
+    }
+
+    public String getPhone_prefix() {
+        return phone_prefix;
+    }
+
+    public void setPhone_prefix(String phone_prefix) {
+        this.phone_prefix = phone_prefix;
+    }
+
+    public String getPhone() {
+        return phone;
+    }
+
+    public void setPhone(String phone) {
+        this.phone = phone;
+    }
+
+    public String getCc_emails() {
+        return cc_emails;
+    }
+
+    public void setCc_emails(String cc_emails) {
+        this.cc_emails = cc_emails;
+    }
+
     @Override
     public String toString() {
-        return "CustomerResponse{" + "id=" + id + ", email=" + email + ", name=" + name + ", notes=" + notes + ", createdAt=" + createdAt + ", updatedAt=" + updatedAt + ", customVariables=" + customVariables + ", cpfCnpj=" + cpfCnpj + '}';
+        return "CustomerResponse{" + "id=" + id + ", email=" + email + ", name=" + name + ", notes=" + notes + ", createdAt=" + createdAt + ", updatedAt=" + updatedAt + ", customVariables=" + customVariables + ", cpfCnpj=" + cpfCnpj + ", zipCode=" + zipCode + ", number=" + number + ", street=" + street + ", district=" + district + ", complement=" + complement + ", city=" + city + ", state=" + state + ", phone_prefix=" + phone_prefix + ", phone=" + phone + ", cc_emails=" + cc_emails + '}';
     }
-    
+
 }
