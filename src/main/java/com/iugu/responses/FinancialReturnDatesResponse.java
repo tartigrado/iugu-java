b1b8c6318f72ad6721f59b0239dfbac023789d32
DanielDPN <danielpereiradonascimento@gmail.com>
2018-11-03 11:16:46 -0300

    Adicionado id em FinancialReturnDatesResponse

--- src/main/java/com/iugu/responses/FinancialReturnDatesResponse.java
+++ src/main/java/com/iugu/responses/FinancialReturnDatesResponse.java
@@ -4,11 +4,20 @@ import com.fasterxml.jackson.annotation.JsonProperty;
 
 public class FinancialReturnDatesResponse {
 
+    private Long id;
     private String installment;
     @JsonProperty("return_date")
     private String returnDate;
     private String status;
     private String amount;
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
 
     public String getInstallment() {
         return installment;
