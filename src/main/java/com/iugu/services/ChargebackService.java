48ef32f98d0a93cc8f5f7989f64340a159fd09d3
DanielDPN <danielpereiradonascimento@gmail.com>
2019-01-25 10:29:17 -0300

    Add ChargebackService Find

--- src/main/java/com/iugu/services/ChargebackService.java
+++ src/main/java/com/iugu/services/ChargebackService.java
@@ -2,6 +2,7 @@ package com.iugu.services;
 
 import com.iugu.IuguConfiguration;
 import com.iugu.exceptions.IuguException;
+import com.iugu.responses.ChargebackResponse;
 import com.iugu.responses.ChargebacksResponse;
 
 import javax.ws.rs.core.Response;
@@ -10,6 +11,7 @@ public class ChargebackService {
 
     private IuguConfiguration iugu;
     private final String FIND_ALL_URL = IuguConfiguration.url("/chargebacks");
+    private final String FIND_URL = IuguConfiguration.url("/chargebacks/%s");
 
     public ChargebackService(IuguConfiguration iugu) {
         this.iugu = iugu;
@@ -30,6 +32,21 @@ public class ChargebackService {
         response.close();
 
         throw new IuguException("Error finding chargebacks!", ResponseStatus, ResponseText);
+    }
+
+    public ChargebackResponse find(String id) throws IuguException {
+        Response response = this.iugu.getNewClient().target(String.format(FIND_URL, id)).request().get();
+        int ResponseStatus = response.getStatus();
+        String ResponseText = null;
+        if (ResponseStatus == 200) {
+            return response.readEntity(ChargebackResponse.class);
+        }
+        // Error Happened
+        if (response.hasEntity()) {
+            ResponseText = response.readEntity(String.class);
+        }
+        response.close();
+        throw new IuguException("Error finding chargeback with id: " + id, ResponseStatus, ResponseText);
     }
 
 }
