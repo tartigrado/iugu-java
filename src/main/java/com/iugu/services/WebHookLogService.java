5228d3a1c1d3eca3193b4fdb94e200ac61472ece
Daniel Pereira <danielpereiradonascimento@gmail.com>
2019-11-27 11:33:10 -0300

    nova funcionalidade para disparar webHookLog

--- src/main/java/com/iugu/services/WebHookLogService.java
+++ src/main/java/com/iugu/services/WebHookLogService.java
@@ -13,6 +13,7 @@ public class WebHookLogService {
 
     private IuguConfiguration iugu;
     private final String FIND_ALL_URL = IuguConfiguration.url("/web_hook_logs/%s");
+    private final String CREATE_URL = IuguConfiguration.url("/web_hook_logs/%s/retry");
 
     public WebHookLogService(IuguConfiguration iugu) {
         this.iugu = iugu;
@@ -35,6 +36,24 @@ public class WebHookLogService {
         response.close();
 
         throw new IuguException("Error finding web_hook logs ", ResponseStatus, ResponseText);
+    }
+
+    public WebHookLogResponse create(String logId) throws IuguException {
+        Response response = this.iugu.getNewClient().target(String.format(CREATE_URL, logId)).request().get();
+
+        int ResponseStatus = response.getStatus();
+        String ResponseText = null;
+
+        if (ResponseStatus == 200)
+            return response.readEntity(WebHookLogResponse.class);
+
+        // Error Happened
+        if (response.hasEntity())
+            ResponseText = response.readEntity(String.class);
+
+        response.close();
+
+        throw new IuguException("Error creating web_hook logs ", ResponseStatus, ResponseText);
     }
 
 }
