8e7a94d2aa232f126492d2ca6c84d20759bd4148
kelly cibely <kelly@tartigrado.com>
2021-01-25 10:13:14 -0300

    alterado iuguException para retornar apenas json de erros

--- src/main/java/com/iugu/exceptions/IuguException.java
+++ src/main/java/com/iugu/exceptions/IuguException.java
@@ -4,6 +4,6 @@ public class IuguException extends Exception {
 	private static final long serialVersionUID = 8701207870125630038L;
 	
 	public IuguException(String Message, int ResponseCode, String ResponseText) {
-		super(Message + " - StatusCode: [" + ResponseCode + "] / ResponseText: [" + ResponseText + "]");
+		super(ResponseText);
 	}
 }
