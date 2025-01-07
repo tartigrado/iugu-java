c0062e14ea6a76de9b21b87ff8523bc3870cdea1
Artur Oliveira <artur@tartigrado.com>
2025-01-07 14:34:04 -0300

    feat: added better exception handler

--- src/main/java/com/iugu/exceptions/IuguExceptionHandler.java
+++ src/main/java/com/iugu/exceptions/IuguExceptionHandler.java
@@ -2,12 +2,16 @@ package com.iugu.exceptions;
 
 import com.iugu.components.Jackson;
 import com.iugu.model.generic.IuguError;
+import jakarta.ws.rs.ProcessingException;
 import jakarta.ws.rs.WebApplicationException;
 import jakarta.ws.rs.core.GenericType;
 import jakarta.ws.rs.core.Response;
+import lombok.extern.log4j.Log4j2;
 
 import java.util.Map;
 
+
+@Log4j2
 public final class IuguExceptionHandler {
     final ExceptionTranslator translator = new ExceptionTranslator();
 
@@ -30,9 +34,14 @@ public final class IuguExceptionHandler {
     IuguError getIuguError(WebApplicationException ex) {
         Map<String, Object> errors;
         try (Response error = ex.getResponse()) {
-            errors = error.readEntity(new GenericType<>() {
-            });
+            try {
+                errors = error.readEntity(new GenericType<>() {
+                });
+            } catch (ProcessingException e) {
+                errors = Map.of("errors", error.readEntity(String.class));
+            }
         }
+        log.error("Iugu error: {}", errors);
         Map<String, Object> translatedErrors = translator.translate(errors);
         return Jackson.getInstance().convertValue(translatedErrors, IuguError.class);
     }
